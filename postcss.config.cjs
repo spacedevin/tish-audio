98b860fa7db2ebea449e9d198f4bb69feb05b503
spacedevin <git@arzynik.com>
2026-04-17 20:14:27 -0700

    ui

---
+++ postcss.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: {
+    tailwindcss: { config: "./tish-tailwind/tailwind.config.cjs" },
+    autoprefixer: {},
+  },
+};
---
+++ tish-tailwind/tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.tish",
+    "./tish-tailwind/src/**/*.tish",
+    "./16.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
